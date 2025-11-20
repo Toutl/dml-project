d2c6e0dd074889a8d6fbcf6c179ba554bc69cd35
gwyndolinds <julioandres2005@gmail.com>
2025-11-20 12:49:06 -0600

    first implementation cnn

--- src/classification/c_3_training.py
+++ src/classification/c_3_training.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.18.1
+#       jupytext_version: 1.17.3
 #   kernelspec:
-#     display_name: dml-project
+#     display_name: venvp
 #     language: python
 #     name: python3
 # ---
@@ -67,7 +67,14 @@ display(metadata_df.head())
 # Files
 processed_images = np.load(processed_folder / "processed_images.npz")
 files = processed_images.files
-print(files[:10])
+print(np.array(files[:15]))
+
+# %%
+imgsused = 15000
+data = np.array([processed_images[i] for i in files[:imgsused]])
+
+# %%
+data = np.array(data)
 
 # %%
 # Ejemplo
@@ -87,13 +94,105 @@ plt.show()
 # ### Modelo 1. CNN:
 
 # %%
-input_size = 2
-output_size = 1
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense, BatchNormalization
+from keras.models import Sequential
+from keras.optimizers import Adam
 
-model = tf.keras.Sequential([tf.keras.layers.Dense(output_size)])
+def build_binary_cnn():
+    model = Sequential()
 
-model.compile(optimizer="sgd", loss="mean_squared_error")
+    model.add(Conv2D(filters=16, kernel_size=3, padding="same", activation="relu",
+                     input_shape=(128, 128, 1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=2))
 
-model.fit(metadata_df["class"], processed_images, epochs=100, verbose="")
+    model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+
+    model.add(Conv2D(filters=64, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+    
+    model.add(Conv2D(filters=128, kernel_size=3, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+
+    model.add(Dropout(0.3))
+
+    model.add(Flatten())
+    
+    model.add(Dense(64, activation="relu")) 
+    model.add(Dropout(0.4))
+
+    model.add(Dense(1, activation="sigmoid"))
+    
+    optimizer = Adam(learning_rate=1e-4)
+    model.compile(loss="binary_crossentropy", optimizer=optimizer,
+                  metrics=["accuracy"])
+    
+    return model
+
+
+# %%
+# from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#Callbacks para el fit del modelo, funcionan para encontrar el mejor epoch
+early_stop = EarlyStopping(
+    monitor='val_loss',
+    patience=10,
+    restore_best_weights=True
+)
+
+# %%
+cnn_model = build_binary_cnn()
+cnn_model.summary()
+
+# %%
+metadata_df['class'].value_counts()
+
+# %%
+from sklearn.model_selection import train_test_split
+
+y = metadata_df['class'][:imgsused].map({"S":0, "E":1, "A":0})
+
+x_train, x_test ,y_train, y_test = train_test_split(data,y, random_state=1, shuffle=True)
+
+x_test = np.array(x_test).astype("float32")
+y_test = np.array(y_test).astype("float32")
+x_train =np.array(x_train).astype("float32")
+y_train= np.array(y_train).astype("float32")
+
+# %%
+y = np.array(y)
+display(data.shape,y[:,np.newaxis].shape)
+
+# %%
+hist_cnn = cnn_model.fit(
+    x_train,
+    y_train,
+    batch_size=32,
+    epochs=20,
+    validation_data=(x_test, y_test),
+    callbacks=[early_stop]
+)
+
+# %%
+plt.plot(hist_cnn.history['accuracy'])
+plt.plot(hist_cnn.history['val_accuracy'])
+plt.title('accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epochs')
+plt.legend(['training', 'validation'], loc='lower right')
+plt.show()
+
+# training vs validation loss
+plt.plot(hist_cnn.history['loss'])
+plt.plot(hist_cnn.history['val_loss'])
+plt.title('loss')
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend(['training', 'validation'], loc='upper right')
+plt.show()
+
+# %%
 
 # %%
